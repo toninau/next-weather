c2e2b762f85cefdd06d387031f1aa6793efb2925
Toni Naumanen <toni.naumis@gmail.com>
2023-07-06 14:46:03 +0300

    Add tailwind prettier plugin

---
+++ prettier.config.js
@@ -0,0 +1,18 @@
+/** @type {import("prettier").Options} */
+const config = {
+  printWidth: 90,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: "as-needed",
+  jsxSingleQuote: false,
+  trailingComma: "all",
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: "avoid",
+  endOfLine: "lf",
+  plugins: [require('prettier-plugin-tailwindcss')],
+}
+
+module.exports = config;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <main>
       <h1>Weather</h1>
+      <button className="px-4 text-white">search</button>
     </main>
   );
 }
